becf6e314835b1ca5ba09c3edec6dba055c352b0
Clément Omnès <clement.mns.pro@gmail.com>
2025-10-17 14:37:05 +0200

    refactor: remove TypeScript error suppression for global CSS import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-// @ts-expect-error -- importing global CSS file for Next app root
 import "./globals.css";
 
 const geistSans = Geist({
